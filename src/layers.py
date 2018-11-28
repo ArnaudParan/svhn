04594510bab3957067a680199d5968b91da2e9d0
ArnaudParanBalto <Arnaud.Paran@homedelivery.com.au>
2018-11-28 18:39:33 +1100

    shape changer layer added

---
+++ src/layers.py
@@ -0,0 +1,23 @@
+from keras import backend as K
+from keras.engine.topology import Layer
+
+class ShapeChanger(Layer):
+
+    def __init__(self, **kwargs):
+        self.output_dim = 66
+        super(MyLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        super(MyLayer, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x):
+        bs = K.shape(x)[0]
+        return K.concat([x[:,:7], [[0. for _ in range(4)] for _ in range(bs)], x[:,7:]])
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
+
+    def get_config(self):
+        config = {}
+        return config
