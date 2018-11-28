ee5baf051e412267261c914f74f7abfc82043590
ArnaudParanBalto <Arnaud.Paran@homedelivery.com.au>
2018-11-28 18:40:31 +1100

    layer corrected

--- src/layers.py
+++ src/layers.py
@@ -5,11 +5,11 @@ class ShapeChanger(Layer):
 
     def __init__(self, **kwargs):
         self.output_dim = 66
-        super(MyLayer, self).__init__(**kwargs)
+        super(ShapeChanger, self).__init__(**kwargs)
 
     def build(self, input_shape):
         # Create a trainable weight variable for this layer.
-        super(MyLayer, self).build(input_shape)  # Be sure to call this at the end
+        super(ShapeChanger, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
         bs = K.shape(x)[0]
